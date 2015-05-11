1079bacac3d87d0334010c70eeba1541fdd3bb44
alu0100777758 <alu0100777758@ull.edu.es>
2015-05-11 12:48:25 +0100

    incluida la interfaz MovementEquation

---
+++ src/es/ull/etsii/pai/practicafinal/physics/ParabolicLocomotion.java
@@ -0,0 +1,35 @@
+package es.ull.etsii.pai.practicafinal.physics;
+
+import es.ull.etsii.pai.prct9.geometry.Point2D;
+
+public class ParabolicLocomotion implements MovementEquation {
+	private int gravity = 3;
+	public ParabolicLocomotion(int gravity){
+		setGravity(gravity);
+	}
+	public int getGravity() {
+		return gravity;
+	}
+
+	public void setGravity(int gravity) {
+		this.gravity = gravity;
+	}
+
+	@Override
+	public Point2D getNewpos(Point2D vector, Point2D oldPos) {
+		Point2D res =  new Point2D(0,0).add(oldPos);
+		vector = vector.add(new Point2D(0, getGravity())); // se aplica aceleración
+		return res.add(vector);
+	}
+
+	@Override
+	public int getNewX(Point2D vector, Point2D oldPos) {
+		return (int) (oldPos.x() + vector.x());
+	}
+
+	@Override
+	public int getNewY(Point2D vector, Point2D oldPos) {
+		return (int) (oldPos.y() + vector.y() + getGravity());
+	}
+
+}
