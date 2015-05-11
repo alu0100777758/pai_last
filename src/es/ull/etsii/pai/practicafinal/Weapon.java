0ab102c34b2e9e4f3fefc4b68d0f7f746b2230f6
Eldon <alu0100777758@edu.ull.es>
2015-05-12 00:34:20 +0100

    trabajado sobre weapon

--- src/es/ull/etsii/pai/practicafinal/Weapon.java
+++ src/es/ull/etsii/pai/practicafinal/Weapon.java
@@ -5,6 +5,57 @@ public class Weapon {
 	private int charger;		// número máximo de balas.
 	private int ChargerBulletCounter; 	// cantidad de balas actualmente en el cargador
 	private int bulletCounter = -1;  // munición disponible, infinito si < 0;
+	boolean pulsedMainTrigger = false;
+	boolean pulsedSecondaryTrigger = false;
+	protected double getFireRate() {
+		return fireRate;
+	}
+	protected void setFireRate(double fireRate) {
+		this.fireRate = fireRate;
+	}
+	protected int getCharger() {
+		return charger;
+	}
+	protected void setCharger(int charger) {
+		this.charger = charger;
+	}
+	protected int getChargerBulletCounter() {
+		return ChargerBulletCounter;
+	}
+	protected void setChargerBulletCounter(int chargerBulletCounter) {
+		ChargerBulletCounter = chargerBulletCounter;
+	}
+	protected int getBulletCounter() {
+		return bulletCounter;
+	}
+	protected void setBulletCounter(int bulletCounter) {
+		this.bulletCounter = bulletCounter;
+	}
+	protected boolean isPulsedMainTrigger() {
+		return pulsedMainTrigger;
+	}
+	protected void setPulsedMainTrigger(boolean pulsedMainTrigger) {
+		this.pulsedMainTrigger = pulsedMainTrigger;
+	}
+	protected boolean isPulsedSecondaryTrigger() {
+		return pulsedSecondaryTrigger;
+	}
+	protected void setPulsedSecondaryTrigger(boolean pulsedSecondaryTrigger) {
+		this.pulsedSecondaryTrigger = pulsedSecondaryTrigger;
+	}
+	
+	public void triggerMain(){
+		setPulsedMainTrigger(true);
+	}
+	public void triggerSecondary(){
+		setPulsedSecondaryTrigger(true);
+	}
+	public void releaseMain(){
+		setPulsedMainTrigger(false);
+	}
+	public void releaseSecondary(){
+		setPulsedSecondaryTrigger(false);
+	}
 	
 	
 }
