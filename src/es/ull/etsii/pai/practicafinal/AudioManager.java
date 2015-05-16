0dabfab6b185194aeb2e63bd12cdb88aaeb1f525
alu0100764666 <alu0100764666@ull.edu.es>
2015-05-16 12:29:18 +0200

    funciona audio linux

--- src/es/ull/etsii/pai/practicafinal/AudioManager.java
+++ src/es/ull/etsii/pai/practicafinal/AudioManager.java
@@ -1,12 +1,36 @@
 package es.ull.etsii.pai.practicafinal;
 
 import java.applet.Applet;
+import java.applet.AudioClip;
 import java.net.URL;
+import java.util.ArrayList;
 
 public class AudioManager {
 	private URL res = getClass().getResource("rocketShot.wav");
+	private ArrayList<AudioClip> clips = new ArrayList<AudioClip>();
 	
 	public void startAudio() {
-		Applet.newAudioClip(res).play();
+		AudioClip audio = Applet.newAudioClip(res);
+		getClips().add(audio);
+		audio.play();
+		
 	}
+
+	public URL getRes() {
+		return res;
+	}
+
+	public void setRes(URL res) {
+		this.res = res;
+	}
+
+	public ArrayList<AudioClip> getClips() {
+		return clips;
+	}
+
+	public void setClips(ArrayList<AudioClip> clips) {
+		this.clips = clips;
+	}
+	
+	
 }
