2adc16427d4ba201e5d0a40cf35cd868b19e482d
Germán Méndez <gcpmendez@gmail.com>
2015-05-16 08:59:41 +0100

    Agregado Sonido (estándares)

--- src/es/ull/etsii/pai/practicafinal/menu/MenuState.java
+++ src/es/ull/etsii/pai/practicafinal/menu/MenuState.java
@@ -1,126 +1,108 @@
 package es.ull.etsii.pai.practicafinal.menu;
 
-
 import java.awt.*;
 import java.awt.event.KeyEvent;
 
 public class MenuState extends GameState {
-	
+
 	private Background bg;
-	
+
 	private int currentChoice = 0;
-	private String[] options = {
-		"Iniciar Partida",
-		"Editar Escenario",
-		"Opciones",
-		"Salir"
-	};
-	
+	private String[] options = { "Iniciar Partida", "Editar Escenario", "Opciones", "Salir" };
+
 	private Color titleColor;
 	private Font titleFont;
-	
+
 	private Font font;
-	
+
 	public MenuState(GameStateManager gsm) {
-		
+
 		this.gsm = gsm;
-		
+
 		try {
-			
+
 			bg = new Background("/Backgrounds/menubg.gif", 1);
 			bg.setVector(-0.1, 0);
-			
+
 			titleColor = new Color(128, 0, 0);
-			titleFont = new Font(
-					"Century Gothic",
-					Font.PLAIN,
-					28);
-			
+			titleFont = new Font("Century Gothic", Font.PLAIN, 28);
+
 			font = new Font("Arial", Font.PLAIN, 12);
-			
-		}
-		catch(Exception e) {
+
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
-	public void init() {}
-	
+
+	public void init() {
+	}
+
 	public void update() {
 		bg.update();
 	}
-	
+
 	public void draw(Graphics2D g) {
-		
+
 		// draw bg
 		bg.draw(g);
-		
+
 		// draw title
 		g.setColor(titleColor);
 		g.setFont(titleFont);
 		g.drawString("Red Vs Blue", 80, 70);
-		
+
 		// draw menu options
 		g.setFont(font);
-		for(int i = 0; i < options.length; i++) {
-			if(i == currentChoice) {
+		for (int i = 0; i < options.length; i++) {
+			if (i == currentChoice) {
 				g.setColor(Color.BLACK);
-			}
-			else {
+			} else {
 				g.setColor(Color.RED);
 			}
 			g.drawString(options[i], 120, 140 + i * 15);
 		}
-		
+
 	}
-	
+
 	private void select() {
-		if(currentChoice == 0) {
+		if (currentChoice == 0) {
 			// Iniciar partida
 		}
-		if(currentChoice == 1) {
+		if (currentChoice == 1) {
 			// Editar
 		}
-		if(currentChoice == 2) {
+		if (currentChoice == 2) {
 			// Opciones
 		}
-		if(currentChoice == 3) {
+		if (currentChoice == 3) {
 			System.exit(0);
 		}
 	}
-	
+
 	public void keyPressed(int k) {
-		if(k == KeyEvent.VK_ENTER){
+		if (k == KeyEvent.VK_ENTER) {
 			select();
 		}
-		if(k == KeyEvent.VK_UP) {
-			Sonidos reproducir = new Sonidos("sonido.mp3");
-	        reproducir.start();
+		if (k == KeyEvent.VK_UP) {
+			Sounds reproducir = new Sounds();
+			reproducir.playSound();
 			currentChoice--;
-			if(currentChoice == -1) {
+			if (currentChoice == -1) {
 				currentChoice = options.length - 1;
 			}
 		}
-		if(k == KeyEvent.VK_DOWN) {
-			Sonidos reproducir = new Sonidos("sonido.mp3");
-	        reproducir.start();
+		if (k == KeyEvent.VK_DOWN) {
+//			Sounds reproducir = new Sounds();
+//			reproducir.start();
 			currentChoice++;
-			if(currentChoice == options.length) {
+			if (currentChoice == options.length) {
 				currentChoice = 0;
 			}
 		}
 	}
-	public void keyReleased(int k) {}
-	
+
+	public void keyReleased(int k) {
+	}
+
 }
-
-
-
-
-
-
-
-
-
-
---
+++ src/es/ull/etsii/pai/practicafinal/menu/prueba.java
@@ -0,0 +1,5 @@
+package es.ull.etsii.pai.practicafinal.menu;
+
+public class prueba {
+
+}
