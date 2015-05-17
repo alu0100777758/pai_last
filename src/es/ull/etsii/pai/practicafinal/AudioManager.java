cf9a9de7c90054c5a3e3414685c3ce962a9b2be9
alu0100764666 <alu0100764666@ull.edu.es>
2015-05-17 17:53:01 +0100

    Sigo con coments

--- src/es/ull/etsii/pai/practicafinal/AudioManager.java
+++ src/es/ull/etsii/pai/practicafinal/AudioManager.java
@@ -1,15 +1,26 @@
 package es.ull.etsii.pai.practicafinal;
-
+/**
+ * Progamacion de aplicaciones interactivas.
+ * Universidad de La Laguna.
+ * 
+ * @author Sabato Ceruso sab7093@gmail.com
+ * @author Javier Martin Hernandez alu0100777758@ull.edu.es
+ *
+ */
 import java.applet.Applet;
 import java.applet.AudioClip;
 import java.net.URL;
 import java.util.ArrayList;
 
 public class AudioManager {
-	public static final int MAX_CONCURRENT_SOUNDS = 15;
-	private URL res = getClass().getResource("/sounds/rocketShot.wav");
-	private static ArrayList<AudioClip> clips = new ArrayList<AudioClip>();	//Truco sucio para engañar al planificador
-	private static ArrayList<AudioClip> loops = new ArrayList<AudioClip>();
+	public static final int MAX_CONCURRENT_SOUNDS = 15;							// Numero maximo de sonidos reproduciendo a la vez.
+	private static ArrayList<AudioClip> clips = new ArrayList<AudioClip>();		// Truco sucio para engañar al planificador
+	private static ArrayList<AudioClip> loops = new ArrayList<AudioClip>();		// Lista de clips ejecutandose indefinidamente.
+	
+	/**
+	 * Comienza la reproduccion del audio indicado por parametro.
+	 * @param name
+	 */
 	public static void startAudio(String name) {
 		AudioClip audio = Applet.newAudioClip( AudioManager.class.getResource("/sounds/"+name));
 		getClips().add(audio);
@@ -19,39 +30,36 @@ public class AudioManager {
 				getClips().remove(0);
 		}
 	}
-	
-	public static ArrayList<AudioClip> getLoops() {
-		return loops;
+	/**
+	 * Pausa todos los clips.
+	 */
+	public static void stopAll() {
+		for(AudioClip audio : getLoops())
+			audio.stop();
 	}
-
-	public static void setLoops(ArrayList<AudioClip> loops) {
-		AudioManager.loops = loops;
-	}
-
+	/**
+	 * Reproduce de forma ciclica el clip con nombre indicado.
+	 * @param name
+	 */
 	public static void reproduceAudio(String name) {
 		AudioClip audio = Applet.newAudioClip( AudioManager.class.getResource("/sounds/"+name));
-//		getClips().add(audio);
 		audio.loop();
 		getLoops().add(audio);
-//		if(getClips().size() >= 2*MAX_CONCURRENT_SOUNDS){
-//			for(int i = 0 ; i<MAX_CONCURRENT_SOUNDS; i++)
-//				getClips().remove(0);
-//		}
+
 	}
+	/**
+	 * Para un determinado clip.
+	 * @param name
+	 */
 	public static void stopAudio(String name) {
 		AudioClip audio = Applet.newAudioClip( AudioManager.class.getResource("/sounds/"+name));
 		audio.stop();
 		getLoops().remove(audio);
 	}
-
-	public URL getRes() {
-		return res;
-	}
-
-	public void setRes(URL res) {
-		this.res = res;
-	}
-
+	/**
+	 * Getters y Setters.
+	 * @return
+	 */
 	public static ArrayList<AudioClip> getClips() {
 		return clips;
 	}
@@ -59,10 +67,12 @@ public class AudioManager {
 	public void setClips(ArrayList<AudioClip> clips) {
 		this.clips = clips;
 	}
-	public static void stopAll() {
-		for(AudioClip audio : getLoops())
-			audio.stop();
-		
+	public static ArrayList<AudioClip> getLoops() {
+		return loops;
+	}
+
+	public static void setLoops(ArrayList<AudioClip> loops) {
+		AudioManager.loops = loops;
 	}
 	
 	
--- src/es/ull/etsii/pai/practicafinal/Entity.java
+++ src/es/ull/etsii/pai/practicafinal/Entity.java
@@ -1,5 +1,12 @@
 package es.ull.etsii.pai.practicafinal;
-
+/**
+ * Progamacion de aplicaciones interactivas.
+ * Universidad de La Laguna.
+ * 
+ * @author Sabato Ceruso sab7093@gmail.com
+ * @author Javier Martin Hernandez alu0100777758@ull.edu.es
+ *
+ */
 import java.awt.Dimension;
 import java.awt.Rectangle;
 import java.awt.geom.RectangularShape;
@@ -7,11 +14,36 @@ import java.io.Serializable;
 
 public abstract class Entity implements Serializable {
 
-	public abstract RectangularShape getShape();
-	public abstract void setLocation(int x, int y);
+	/**
+	 * Obtiene la forma de la entidad.
+	 * @return
+	 */
+	public abstract RectangularShape getShape();	
+	/**
+	 * Cambia la posicion de la entidad.
+	 * @param x
+	 * @param y
+	 */
+	public abstract void setLocation(int x, int y);	
+	/**
+	 * Obtiene la coordenada x de la posicion.
+	 * @return
+	 */
 	public abstract int getX();
+	/**
+	 * Obtiene la coordenada y de la posicion.
+	 * @return
+	 */
 	public abstract int gety();
+	/**
+	 * Modifica el tamaño de la entidad.
+	 * @param width
+	 * @param height
+	 */
 	public abstract void setSize(int width, int height);
+	/**
+	 * Devuelve una copia de la entidad.
+	 */
 	public Object clone(){
 		try {
 			return super.clone();
