b9abc2926c663a4da9522198850d44ba115712e0
SCeruso <sab7093@gmail.com>
2015-07-15 21:47:33 +0100

    Ahora esta mas bonito

--- src/es/ull/etsii/pai/practicafinal/main/MapSelector.java
+++ src/es/ull/etsii/pai/practicafinal/main/MapSelector.java
@@ -3,14 +3,17 @@ package es.ull.etsii.pai.practicafinal.main;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Graphics;
 import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 
+import javax.imageio.ImageIO;
 import javax.swing.Box;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
@@ -29,7 +32,7 @@ public class MapSelector extends ScenarioPanel implements ActionListener {
 	public static final double BOTTOM_MARGIN = 0.25;
 	public static final double SIDE_MARGINS = 0.10;
 	public static final double GAP_BETWEEN_LEVELS = 0.05;
-	
+	private BufferedImage img;
 	private static final String MAPS_PATH = System.getProperty("user.dir")
 			+ System.getProperty("file.separator") + "maps";
 	
@@ -42,8 +45,22 @@ public class MapSelector extends ScenarioPanel implements ActionListener {
 	public MapSelector() {
 		scanDir();
 		fillGrid();
+		
+		
+		try {
+			img = ImageIO.read(new File ("Recursos/textures/metal.png"));
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
 
+	protected void paintComponent (Graphics g) {
+		super.paintComponent(g.create());
+		
+		g.drawImage(img, 0, 0, ScreenManager.getInstance().getCurrentWidth(), ScreenManager.getInstance().getCurrentHeight(), this);
+	}
 	private void fillGrid() {
 		setLayout(new BorderLayout());
 		
@@ -162,7 +179,9 @@ public class MapSelector extends ScenarioPanel implements ActionListener {
 	class bottomLayer extends JPanel {
 		public bottomLayer() {
 			ScreenManager screen = ScreenManager.getInstance();
-			setLayout(new GridLayout(3, 1));
+			setLayout(new BorderLayout());
+			JPanel gridPanel = new JPanel(new GridLayout(3, 1));
+			gridPanel.setOpaque(false);
 			GridBagConstraints c = new GridBagConstraints();
 			JPanel description = new JPanel();
 			description.setBackground(Color.GREEN);
@@ -171,41 +190,30 @@ public class MapSelector extends ScenarioPanel implements ActionListener {
 			description.setLayout(new GridLayout(4, 4));
 			JButton leftArrow = new JButton();
 			JButton rightArrow = new JButton();
-		/*	c.fill = GridBagConstraints.NONE;
-			c.weightx = 0.0;
-			c.weighty = 0.2;
-			c.anchor = GridBagConstraints.WEST;
-			*/
+
 			leftArrow.setIcon(new ImageIcon(ResourceManager.getInstance()
 					.getBufferedImage("icons/return.png")));
-			//add(leftArrow, c);
 			leftArrow.setActionCommand("return");
 			leftArrow.addActionListener(MapSelector.this);
-		/*	c.anchor = GridBagConstraints.CENTER;
-			c.fill = GridBagConstraints.BOTH;
-			c.weightx = 0.5;
-			c.weighty = 0.5;
-			
-		//	add(description, c);
-			c.fill = GridBagConstraints.VERTICAL;
-			c.weightx = 0.0;
-			c.weighty = 0.2;
-			c.anchor = GridBagConstraints.EAST;
-			*/
+
 			rightArrow.setActionCommand("play");
 			rightArrow.addActionListener(MapSelector.this);
 			rightArrow.setIcon(new ImageIcon(ResourceManager.getInstance()
 					.getBufferedImage("icons/play.png")));
-		//	add(rightArrow, c);
 		
-			add(Box.createHorizontalStrut(1));
+			gridPanel.add(Box.createHorizontalStrut(1));
 			JPanel content = new JPanel(new BorderLayout());
 			content.add(leftArrow, BorderLayout.WEST);
 			content.add(description, BorderLayout.CENTER);
 			content.add(rightArrow, BorderLayout.EAST);
 			content.setOpaque(false);
-			add(content);
-			add(Box.createHorizontalStrut(1));
+			gridPanel.add(Box.createHorizontalStrut(1));
+			gridPanel.add(content);
+			
+			add(gridPanel, BorderLayout.CENTER);
+			add(Box.createHorizontalStrut((int)(screen.getCurrentWidth() * GAP_BETWEEN_LEVELS / 2)), BorderLayout.WEST);  
+			add(Box.createHorizontalStrut((int)(screen.getCurrentWidth() * GAP_BETWEEN_LEVELS / 2)), BorderLayout.EAST);  
+			add(Box.createVerticalStrut((int)(screen.getCurrentHeight() * GAP_BETWEEN_LEVELS / 2)), BorderLayout.SOUTH);  
 			this.setMinimumSize(new Dimension(screen.getCurrentWidth(),(int)(screen.getCurrentHeight() * BOTTOM_MARGIN)));
 			this.setMaximumSize(new Dimension(screen.getCurrentWidth(),(int)(screen.getCurrentHeight() * BOTTOM_MARGIN)));
 			this.setPreferredSize(new Dimension(screen.getCurrentWidth(),(int)(screen.getCurrentHeight() * BOTTOM_MARGIN)));
